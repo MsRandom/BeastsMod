d6c45f17b6858be9614f8c03335829230d994680
CodaBear1552 <46657000+CodaBear1552@users.noreply.github.com>
2019-08-20 23:02:00 -0600

    Vile Eel and Landwhale

---
+++ src/main/java/rando/beasts/common/entity/EntityLandwhale.java
@@ -0,0 +1,112 @@
+package rando.beasts.common.entity;
+
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.annotation.Nonnull;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIFollowParent;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMate;
+import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAITempt;
+import net.minecraft.entity.ai.EntityAIWanderAvoidWater;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.Item;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import rando.beasts.common.init.BeastsItems;
+
+public class EntityLandwhale extends EntityAnimal
+{
+    public EntityLandwhale(World worldIn)
+    {
+        super(worldIn);
+        this.setSize(1.8F, 2.0F);
+    }
+
+    protected void initEntityAI()
+    {
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIPanic(this, 2.0D));
+        this.tasks.addTask(2, new EntityAIMate(this, 1.0D));
+        this.tasks.addTask(3, new EntityAITempt(this, 1.25D, BeastsItems.COCONUT, false));
+        this.tasks.addTask(4, new EntityAIFollowParent(this, 1.25D));
+        this.tasks.addTask(5, new EntityAIWanderAvoidWater(this, 1.0D));
+        this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+        this.tasks.addTask(7, new EntityAILookIdle(this));
+    }
+
+    @Override
+    protected Item getDropItem() {
+        return Items.LEATHER;
+    }
+
+    @Override
+    protected void dropFewItems(boolean wasRecentlyHit, int lootingModifier) {
+        Item leather = this.getDropItem();
+        Item chop = BeastsItems.EEL_CHOP;
+        int i = this.rand.nextInt(4);
+        if (lootingModifier > 0) i += this.rand.nextInt(lootingModifier + 1);
+        for (int j = 0; j < i; ++j) this.dropItem(chop, 1);
+        this.dropItem(Objects.requireNonNull(chop), 1);
+    }
+    
+    @Override
+    public void onUpdate() {
+    	super.onUpdate();
+    }
+    
+    protected void applyEntityAttributes()
+    {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(60.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.18D);
+    }
+
+    protected SoundEvent getAmbientSound()
+    {
+        return null;
+    }
+
+    protected SoundEvent getHurtSound(DamageSource damageSourceIn)
+    {
+        return null;
+    }
+
+    protected SoundEvent getDeathSound()
+    {
+        return null;
+    }
+
+    protected void playStepSound(BlockPos pos, Block blockIn)
+    {
+        this.playSound(SoundEvents.ENTITY_COW_STEP, 0.15F, 1.0F);
+    }
+
+
+    protected float getSoundVolume()
+    {
+        return 0.4F;
+    }
+
+    public EntityLandwhale createChild(EntityAgeable ageable)
+    {
+        return new EntityLandwhale(this.world);
+    }
+
+    public float getEyeHeight()
+    {
+        return this.isChild() ? this.height : 2.0F;
+    }
+}
---
+++ src/main/java/rando/beasts/common/entity/EntityVileEel.java
@@ -0,0 +1,130 @@
+package rando.beasts.common.entity;
+
+import java.util.Objects;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackMelee;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWanderAvoidWater;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.entity.passive.EntityHorse;
+import net.minecraft.entity.passive.EntityMooshroom;
+import net.minecraft.entity.passive.EntityPig;
+import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.Item;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.storage.loot.LootTableList;
+import rando.beasts.common.init.BeastsItems;
+
+public class EntityVileEel extends EntityMob
+{
+    public EntityVileEel(World worldIn)
+    {
+        super(worldIn);
+        this.setSize(1.5F, 1.8F);
+    }
+
+    protected void initEntityAI()
+    {
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityCow.class, true));
+        this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityPig.class, true));
+        this.targetTasks.addTask(4, new EntityAINearestAttackableTarget(this, EntitySheep.class, true));
+        this.targetTasks.addTask(5, new EntityAINearestAttackableTarget(this, EntityChicken.class, true));
+        this.targetTasks.addTask(6, new EntityAINearestAttackableTarget(this, EntityHorse.class, true));
+        this.targetTasks.addTask(7, new EntityAINearestAttackableTarget(this, EntityMooshroom.class, true));
+        this.targetTasks.addTask(8, new EntityAINearestAttackableTarget(this, EntityPufferfishDog.class, true));
+        this.targetTasks.addTask(9, new EntityAINearestAttackableTarget(this, EntityCoconutCrab.class, true));
+        this.tasks.addTask(11, new EntityAIAttackMelee(this, 1.1D, true));
+        this.tasks.addTask(12, new EntityAIWanderAvoidWater(this, 1.0D));
+        this.tasks.addTask(13, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+        this.tasks.addTask(14, new EntityAILookIdle(this));
+    }
+
+    @Override
+    public boolean attackEntityAsMob(Entity entityIn) {
+    	entityIn.attackEntityFrom(DamageSource.causeMobDamage(this), 2.0F);
+    	return super.attackEntityAsMob(entityIn);
+    }
+   
+    @Override
+    protected Item getDropItem() {
+        return Items.LEATHER;
+    }
+
+    @Override
+    protected void dropFewItems(boolean wasRecentlyHit, int lootingModifier) {
+        Item leather = this.getDropItem();
+        Item chop = BeastsItems.EEL_CHOP;
+        int i = this.rand.nextInt(4);
+        if (lootingModifier > 0) i += this.rand.nextInt(lootingModifier + 1);
+        for (int j = 0; j < i; ++j) this.dropItem(chop, 1);
+        this.dropItem(Objects.requireNonNull(chop), 1);
+    }
+    
+    @Override
+    public void onUpdate() {
+    	super.onUpdate();
+    }
+    
+    protected void applyEntityAttributes()
+    {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(100.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.3D);
+    }
+
+    protected SoundEvent getAmbientSound()
+    {
+        return null;
+    }
+
+    protected SoundEvent getHurtSound(DamageSource damageSourceIn)
+    {
+        return null;
+    }
+
+    protected SoundEvent getDeathSound()
+    {
+        return null;
+    }
+
+    protected void playStepSound(BlockPos pos, Block blockIn)
+    {
+        this.playSound(SoundEvents.ENTITY_ZOMBIE_STEP, 0.15F, 1.0F);
+    }
+
+
+    protected float getSoundVolume()
+    {
+        return 0.4F;
+    }
+
+    public EntityVileEel createChild(EntityAgeable ageable)
+    {
+        return new EntityVileEel(this.world);
+    }
+
+    public float getEyeHeight()
+    {
+        return this.isChild() ? this.height : 1.3F;
+    }
+}
