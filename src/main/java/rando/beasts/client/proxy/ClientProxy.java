cc189e0277bea28ceedd0334e5235b189df358c0
CodaBear1552 <46657000+CodaBear1552@users.noreply.github.com>
2019-08-20 23:02:53 -0600

    Updated Client Proxy

--- src/main/java/rando/beasts/client/proxy/ClientProxy.java
+++ src/main/java/rando/beasts/client/proxy/ClientProxy.java
@@ -3,15 +3,17 @@ package rando.beasts.client.proxy;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import rando.beasts.client.renderer.entity.RenderCoconutCrab;
-import rando.beasts.client.renderer.entity.RenderMonkeyGremlin;
+import rando.beasts.client.renderer.entity.RenderLandwhale;
 import rando.beasts.client.renderer.entity.RenderPufferfishDog;
 import rando.beasts.client.renderer.entity.RenderRabbitman;
+import rando.beasts.client.renderer.entity.RenderVileEel;
 import rando.beasts.client.renderer.tileentity.TileEntityCoconutRenderer;
 import rando.beasts.common.entity.EntityCoconutCrab;
-import rando.beasts.common.proxy.CommonProxy;
-import rando.beasts.common.entity.EntityMonkeyGremlin;
+import rando.beasts.common.entity.EntityLandwhale;
 import rando.beasts.common.entity.EntityPufferfishDog;
 import rando.beasts.common.entity.EntityRabbitman;
+import rando.beasts.common.entity.EntityVileEel;
+import rando.beasts.common.proxy.CommonProxy;
 import rando.beasts.common.tileentity.TileEntityCoconut;
 
 @SuppressWarnings("unused")
@@ -25,10 +27,12 @@ public class ClientProxy extends CommonProxy {
 
 	private void registerRenders() {
 		RenderingRegistry.registerEntityRenderingHandler(EntityCoconutCrab.class, RenderCoconutCrab::new);
-		RenderingRegistry.registerEntityRenderingHandler(EntityMonkeyGremlin.class, RenderMonkeyGremlin::new);
 		RenderingRegistry.registerEntityRenderingHandler(EntityPufferfishDog.class, RenderPufferfishDog::new);
 		RenderingRegistry.registerEntityRenderingHandler(EntityRabbitman.class, RenderRabbitman::new);
+		RenderingRegistry.registerEntityRenderingHandler(EntityVileEel.class, RenderVileEel::new);
+		RenderingRegistry.registerEntityRenderingHandler(EntityLandwhale.class, RenderLandwhale::new);
 
+		
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityCoconut.class, new TileEntityCoconutRenderer());
 	}
 }
