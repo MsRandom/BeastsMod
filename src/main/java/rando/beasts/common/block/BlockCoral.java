f55bf977dec73b59edab5cd6bb0e386c013831fd
D4RSORC <42446985+D4RSORC@users.noreply.github.com>
2019-08-24 00:11:53 +0200

    Coral essences now drop from corals

--- src/main/java/rando/beasts/common/block/BlockCoral.java
+++ src/main/java/rando/beasts/common/block/BlockCoral.java
@@ -1,18 +1,65 @@
 package rando.beasts.common.block;
 
-import net.minecraft.block.SoundType;
-import net.minecraft.block.material.MapColor;
-import net.minecraft.block.material.Material;
-import net.minecraft.util.IStringSerializable;
+import static rando.beasts.common.init.BeastsItems.CORAL_ESSENCE_BLUE;
+import static rando.beasts.common.init.BeastsItems.CORAL_ESSENCE_PINK;
+import static rando.beasts.common.init.BeastsItems.CORAL_ESSENCE_PURPLE;
+import static rando.beasts.common.init.BeastsItems.CORAL_ESSENCE_RED;
+import static rando.beasts.common.init.BeastsItems.CORAL_ESSENCE_YELLOW;
+
+import java.util.Random;
 
 import javax.annotation.Nonnull;
 
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.math.MathHelper;
+
 public class BlockCoral extends BeastsBlock {
+	private Item itemToDrop;
+	
     public BlockCoral(Color color) {
         super(Material.PLANTS, color.mapColor, "coral_block_" + color.getName());
         setHardness(0.6F);
         setSoundType(SoundType.PLANT);
+        this.itemToDrop = getCoralEssence(color);
     }
+    
+    public int quantityDroppedWithBonus(int fortune, Random random)
+    {
+        return MathHelper.clamp(this.quantityDropped(random) + random.nextInt(fortune + 1), 1, 4);
+    }
+
+    public int quantityDropped(Random random)
+    {
+        return 2 + random.nextInt(3);
+    }
+
+    public Item getItemDropped(IBlockState state, Random rand, int fortune)
+    {
+        return this.itemToDrop;
+    }
+    
+    private Item getCoralEssence(Color color)
+    {
+    	switch(color) {
+    	case BLUE:
+    		return CORAL_ESSENCE_BLUE;
+    	case PINK:
+    		return CORAL_ESSENCE_PINK;
+    	case PURPLE:
+    		return CORAL_ESSENCE_PURPLE;
+    	case RED:
+    		return CORAL_ESSENCE_RED;
+    	case YELLOW:
+    		return CORAL_ESSENCE_YELLOW;
+    	}
+        return Item.getItemFromBlock(this);
+    }
+    
 
     public enum Color implements IStringSerializable {
         BLUE("blue", MapColor.BLUE),
@@ -32,6 +79,7 @@ public class BlockCoral extends BeastsBlock {
         @Nonnull
         @Override
         public String getName() {
+        	
             return name;
         }
     }
