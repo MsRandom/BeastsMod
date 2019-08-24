c07cc41fa3778dea7ac90ce1868f84de1759514b
D4RSORC <42446985+D4RSORC@users.noreply.github.com>
2019-08-24 19:01:38 +0200

    Added name-easter egg for the pufferpup
    Name the pufferpup "Bufford" and it will wear glasses

--- src/main/java/rando/beasts/client/renderer/entity/RenderPufferfishDog.java
+++ src/main/java/rando/beasts/client/renderer/entity/RenderPufferfishDog.java
@@ -1,22 +1,24 @@
 package rando.beasts.client.renderer.entity;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
-import rando.beasts.common.utils.BeastsReference;
 import rando.beasts.client.model.ModelPufferFishDog;
 import rando.beasts.client.renderer.entity.layers.LayerCollar;
+import rando.beasts.client.renderer.entity.layers.LayerGlasses;
 import rando.beasts.common.entity.passive.EntityPufferfishDog;
-
-import javax.annotation.Nullable;
+import rando.beasts.common.utils.BeastsReference;
 
 public class RenderPufferfishDog extends RenderLiving<EntityPufferfishDog> {
 
     private static final ResourceLocation TEXTURE_NORMAL = new ResourceLocation(BeastsReference.ID, "textures/entity/pufferfish_dog/normal.png");
     private static final ResourceLocation TEXTURE_INFLATED = new ResourceLocation(BeastsReference.ID, "textures/entity/pufferfish_dog/inflated.png");
-
+    
     public RenderPufferfishDog(RenderManager manager) {
         super(manager, new ModelPufferFishDog(), 0.3f);
+        this.addLayer(new LayerGlasses(this));
         this.addLayer(new LayerCollar(this));
     }
 
---
+++ src/main/java/rando/beasts/client/renderer/entity/layers/LayerGlasses.java
@@ -0,0 +1,29 @@
+package rando.beasts.client.renderer.entity.layers;
+
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.util.ResourceLocation;
+import rando.beasts.client.renderer.entity.RenderPufferfishDog;
+import rando.beasts.common.entity.passive.EntityPufferfishDog;
+import rando.beasts.common.utils.BeastsReference;
+
+public class LayerGlasses implements LayerRenderer<EntityPufferfishDog> {
+    private static final ResourceLocation BUFFORD = new ResourceLocation(BeastsReference.ID, "textures/entity/pufferfish_dog/bufford.png");
+    private static final ResourceLocation BUFFORD_INFLATED = new ResourceLocation(BeastsReference.ID, "textures/entity/pufferfish_dog/bufford_inflated.png");
+    private final RenderPufferfishDog render;
+
+    public LayerGlasses(RenderPufferfishDog render) {
+        this.render = render;
+    }
+
+    public void doRenderLayer(EntityPufferfishDog entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+    	if (entity.hasCustomName() && "Bufford".equals(entity.getCustomNameTag()) && !entity.isInvisible())
+        {
+    		this.render.bindTexture(entity.getInflated()?BUFFORD_INFLATED:BUFFORD);
+            this.render.getMainModel().render(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+        }
+    }
+
+    public boolean shouldCombineTextures() {
+        return true;
+    }
+}
