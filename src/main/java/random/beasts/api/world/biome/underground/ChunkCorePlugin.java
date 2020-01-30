4fb42f9d97f063fa87ffe657bafeadfb8ad337ff
MsRandom1 <ahmedtrg981106@gmail.com>
2020-01-30 09:52:44 +0300

    What the asm code is actually supposed to be

--- src/main/java/random/beasts/api/world/biome/underground/ChunkCorePlugin.java
+++ src/main/java/random/beasts/api/world/biome/underground/ChunkCorePlugin.java
@@ -5,8 +5,8 @@ import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import org.apache.logging.log4j.LogManager;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.*;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
@@ -50,33 +50,18 @@ public class ChunkCorePlugin implements IFMLLoadingPlugin {
 
                     cls.methods.stream().filter(v -> v.name.equals("getBiome") || v.name.equals("func_177411_a")).findAny().ifPresent(method -> {
                         InsnList instructions = method.instructions;
-                            /*for (int i = 0; i < instructions.size(); i++) {
-                                System.out.println(instructions.get(i));
-                            }
-                            LabelNode label = (LabelNode)instructions.getFirst();
-                            instructions.insertBefore(label, new InsnNode(Opcodes.ARETURN));
-                            instructions.insertBefore(instructions.getFirst(), new VarInsnNode(Opcodes.ALOAD, 3));
-                            instructions.insertBefore(instructions.getFirst(), new JumpInsnNode(Opcodes.IFNULL, label));
-                            instructions.insertBefore(instructions.getFirst(), new VarInsnNode(Opcodes.ALOAD, 3));
-                            label = new LabelNode();
-                            instructions.insertBefore(instructions.getFirst(), new LineNumberNode(1297, label));
-                            instructions.insertBefore(instructions.getFirst(), label);
-                            */
-                        //instructions.insertBefore(instructions.getFirst(), new VarInsnNode(Opcodes.ASTORE, 3));
-                        //instructions.insertBefore(instructions.getFirst(), new MethodInsnNode(Opcodes.INVOKESTATIC, "random/beasts/api/world/biome/UndergroundBiome", "getBiome", "(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/chunk/Chunk;)Lnet/minecraft/world/biome/Biome", false));
-                        //instructions.insertBefore(instructions.getFirst(), new VarInsnNode(Opcodes.ALOAD, 0));
-                        //instructions.insertBefore(instructions.getFirst(), new VarInsnNode(Opcodes.ALOAD, 1));
-                            /*
-                            System.out.println("TRANSFORMED");
-                            for (int i = 0; i < instructions.size(); i++) {
-                                System.out.println(instructions.get(i));
-                            }*/
+                        LabelNode label = (LabelNode) instructions.getFirst();
+                        instructions.insertBefore(label, new InsnNode(Opcodes.ARETURN));
+                        instructions.insertBefore(instructions.getFirst(), new VarInsnNode(Opcodes.ALOAD, 3));
+                        instructions.insertBefore(instructions.getFirst(), new JumpInsnNode(Opcodes.IFNULL, label));
+                        instructions.insertBefore(instructions.getFirst(), new VarInsnNode(Opcodes.ALOAD, 3));
+                        instructions.insertBefore(instructions.getFirst(), new VarInsnNode(Opcodes.ASTORE, 3));
+                        instructions.insertBefore(instructions.getFirst(), new MethodInsnNode(Opcodes.INVOKESTATIC, "random/beasts/api/world/biome/UndergroundBiome", "getBiome", "(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/chunk/Chunk;)net/minecraft/world/biome/Biome", false));
+                        instructions.insertBefore(instructions.getFirst(), new VarInsnNode(Opcodes.ALOAD, 0));
+                        instructions.insertBefore(instructions.getFirst(), new VarInsnNode(Opcodes.ALOAD, 1));
                     });
                     ClassWriter writer = new ClassWriter(reader, 0);
                     cls.accept(writer);
-                    //FileOutputStream w = new FileOutputStream("/home/random/IdeaProjects/BeastsMod/c.class");
-                    //w.write(writer.toByteArray());
-                    //file:///home/random/Downloads/Programs/mods/RTG-1.12.2-6.1.0.0-snapshot.1-deobf.jarw.close();
                     return writer.toByteArray();
                 } catch (IOException e) {
                     LogManager.getLogger().error("Failed to transform Chunk class, underground biomes won't work.", e);
--- src/main/java/random/beasts/common/init/BeastsEntities.java
+++ src/main/java/random/beasts/common/init/BeastsEntities.java
@@ -1,14 +1,5 @@
 package random.beasts.common.init;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Function;
-
-import org.apache.commons.lang3.StringUtils;
-
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EnumCreatureType;
@@ -19,27 +10,18 @@ import net.minecraft.world.biome.Biome;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.registry.EntityEntry;
 import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
+import org.apache.commons.lang3.StringUtils;
 import random.beasts.api.entity.BeastsBranchie;
 import random.beasts.api.main.BeastsReference;
 import random.beasts.common.entity.item.EntityBeastsPainting;
 import random.beasts.common.entity.item.EntityFallingCoconut;
 import random.beasts.common.entity.item.EntityThrownCoconut;
-import random.beasts.common.entity.monster.EntityChorusBranchie;
-import random.beasts.common.entity.monster.EntityCoconutCrab;
-import random.beasts.common.entity.monster.EntityCoralBranchie;
-import random.beasts.common.entity.monster.EntityGiantGardenEel;
-import random.beasts.common.entity.monster.EntityScallop;
-import random.beasts.common.entity.monster.EntitySkewerShrimp;
-import random.beasts.common.entity.monster.EntityTrimola;
-import random.beasts.common.entity.monster.EntityVileEel;
-import random.beasts.common.entity.monster.EntityWoodBranchie;
-import random.beasts.common.entity.passive.EntityAnemoneCrawler;
-import random.beasts.common.entity.passive.EntityHermitTurtle;
-import random.beasts.common.entity.passive.EntityLandwhale;
-import random.beasts.common.entity.passive.EntityLegfish;
-import random.beasts.common.entity.passive.EntityPufferfishDog;
-import random.beasts.common.entity.passive.EntitySlimeSlug;
+import random.beasts.common.entity.monster.*;
+import random.beasts.common.entity.passive.*;
 import random.beasts.common.entity.projectile.EntityCoconutBomb;
+
+import java.util.*;
+import java.util.function.Function;
 
 @SuppressWarnings("unused")
 public class BeastsEntities {
@@ -47,7 +29,7 @@ public class BeastsEntities {
     public static final List<EntityEntry> LIST = new ArrayList<>();
 
     private static int entityId = 0;
-    private static final EntityEntry PUFFERFISH_DOG = createEntry(EntityPufferfishDog.class, 0xFBA70C, 0x429BBA, new SpawnEntry(EnumCreatureType.CREATURE, 25, 1, 1, BeastsBiomes.DRIED_REEF));
+    private static final EntityEntry PUFFERFISH_DOG = createEntry(EntityPufferfishDog.class, 0xFBA70C, 0x429BBA, new SpawnEntry(EnumCreatureType.CREATURE, 30, 1, 1, BeastsBiomes.DRIED_REEF));
     //private static final EntityEntry RABBITMAN = createEntry(EntityRabbitman.class, 0x4E362D, 0xE5E5E5);
     private static final EntityEntry COCONUT_CRAB = createEntry(EntityCoconutCrab.class, 0x3C1C11, 0xA16745);
     private static final EntityEntry CORAL_BRANCHIE = createBranchie(EntityCoralBranchie.class, 0xEDEC4C, 0xD6549B, BeastsBlocks.CORAL_PLANTS.values(), new Tuple<>(2, EntityCoralBranchie::create));
@@ -56,15 +38,15 @@ public class BeastsEntities {
     private static final EntityEntry VILE_EEL = createEntry(EntityVileEel.class, 0x313337, 0x987CAF, new SpawnEntry(EnumCreatureType.MONSTER, 40, 1, 1, BeastsBiomes.DRIED_REEF));
     private static final EntityEntry LANDWHALE = createEntry(EntityLandwhale.class, 0x587377, 0xE25AA5, new SpawnEntry(EnumCreatureType.CREATURE, 30, 1, 1, BeastsBiomes.DRIED_REEF));
     private static final EntityEntry COCONADE = createEntry(EntityCoconutBomb.class);
-    private static final EntityEntry GIANT_GARDEN_EEL = createEntry(EntityGiantGardenEel.class, 0xCECEAF, 0x7A745E, new SpawnEntry(EnumCreatureType.CREATURE, 15, 4, 8, BeastsBiomes.DRIED_REEF));
-    private static final EntityEntry SKEWER_SHRIMP = createEntry(EntitySkewerShrimp.class, 0xEA4E3C, 0xFFACA3, new SpawnEntry(EnumCreatureType.CREATURE, 15, 4, 8, BeastsBiomes.DRIED_REEF));
+    private static final EntityEntry GIANT_GARDEN_EEL = createEntry(EntityGiantGardenEel.class, 0xCECEAF, 0x7A745E, new SpawnEntry(EnumCreatureType.CREATURE, 30, 4, 8, BeastsBiomes.DRIED_REEF));
+    private static final EntityEntry SKEWER_SHRIMP = createEntry(EntitySkewerShrimp.class, 0xEA4E3C, 0xFFACA3, new SpawnEntry(EnumCreatureType.CREATURE, 30, 4, 8, BeastsBiomes.DRIED_REEF));
     private static final EntityEntry BEASTS_PAINTING = createEntry(EntityBeastsPainting.class);
     private static final EntityEntry FALLING_COCONUT = createEntry(EntityFallingCoconut.class);
     //private static final EntityEntry WHIPPING_BARNACLE = createEntry(EntityWhippingBarnacle.class, 0x278E6C, 0xEA8F9E, new SpawnEntry(EnumCreatureType.CREATURE, 50, 1, 3, BeastsBiomes.DRIED_REEF));
-    private static final EntityEntry HERMIT_TURTLE = createEntry(EntityHermitTurtle.class, 0x8F8F8F, 0x63862E, new SpawnEntry(EnumCreatureType.CREATURE, 40, 1, 3, BeastsBiomes.DRIED_REEF));
+    private static final EntityEntry HERMIT_TURTLE = createEntry(EntityHermitTurtle.class, 0x8F8F8F, 0x63862E, new SpawnEntry(EnumCreatureType.CREATURE, 30, 1, 3, BeastsBiomes.DRIED_REEF));
     private static final EntityEntry ANEMONE_CRAWLER = createEntry(EntityAnemoneCrawler.class, 0xEF5C13, 0xDCE3E5);
-    private static final EntityEntry LEGFISH = createEntry(EntityLegfish.class, 0x6DAF78, 0xDB3E00, new SpawnEntry(EnumCreatureType.CREATURE, 40, 2, 4, BeastsBiomes.DRIED_REEF));
-    private static final EntityEntry SCALLOP = createEntry(EntityScallop.class, 0xE8C7A7, 0x7C3318, new SpawnEntry(EnumCreatureType.MONSTER, 40, 2, 4, BeastsBiomes.DRIED_REEF));
+    private static final EntityEntry LEGFISH = createEntry(EntityLegfish.class, 0x6DAF78, 0xDB3E00, new SpawnEntry(EnumCreatureType.CREATURE, 30, 2, 4, BeastsBiomes.DRIED_REEF));
+    private static final EntityEntry SCALLOP = createEntry(EntityScallop.class, 0xE8C7A7, 0x7C3318, new SpawnEntry(EnumCreatureType.MONSTER, 30, 2, 4, BeastsBiomes.DRIED_REEF));
     private static final EntityEntry TRIMOLA = createEntry(EntityTrimola.class, 0x261E1B, 0xA09783, new SpawnEntry(EnumCreatureType.CREATURE, 20, 2, 4, BeastsBiomes.DRIED_REEF));
     private static final EntityEntry SLIME_SLUG = createEntry(EntitySlimeSlug.class, 0xD3524C, 0xFFDB66, new SpawnEntry(EnumCreatureType.CREATURE, 20, 2, 4, BeastsBiomes.DRIED_REEF));
     private static final EntityEntry THROWN_COCONUT = createEntry(EntityThrownCoconut.class);
