d6a37d95e5ab6b6af0689893591419bcca4d409d
pale-imitations <57423918+pale-imitations@users.noreply.github.com>
2020-02-20 18:28:23 -0700

    Add files via upload

--- src/main/java/random/beasts/common/init/BeastsBiomes.java
+++ src/main/java/random/beasts/common/init/BeastsBiomes.java
@@ -4,8 +4,8 @@ import net.minecraft.world.WorldType;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.common.BiomeManager;
-import random.beasts.api.configuration.BeastsConfig;
 import random.beasts.api.world.biome.underground.UndergroundBiome;
+import random.beasts.common.world.biome.BiomeAbyss;
 import random.beasts.common.world.biome.BiomeDriedReef;
 import random.beasts.common.world.types.WorldTypeBeasts;
 
@@ -13,7 +13,7 @@ public class BeastsBiomes {
 
     public static final WorldType BEASTS_WORLD_TYPE = new WorldTypeBeasts();
     public static final Biome DRIED_REEF = new BiomeDriedReef();
-    public static final UndergroundBiome THE_ABYSS = new UndergroundBiome("The Abyss", BeastsConfig.abyssWeight, DRIED_REEF);
+    public static final UndergroundBiome THE_ABYSS = new BiomeAbyss(DRIED_REEF);
 
     public static void addTypes(Biome biome, BiomeManager.BiomeType mainType, int weight, boolean stronghold, boolean village, BiomeDictionary.Type... types) {
         BiomeManager.addBiome(mainType, new BiomeManager.BiomeEntry(biome, weight));
